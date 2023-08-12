5863a100cfc6edccc4c99035e148397a616f4d94
Rodolfo Pessanha <rodolfo.silva@solides.com.br>
2023-08-12 10:52:31 -0300

    chore(lib): create a lib and deploy

---
+++ src/components/Button/types.ts
@@ -0,0 +1,6 @@
+export interface IButton {
+  children: React.ReactNode;
+  disabled?: boolean;
+  loading?: boolean;
+  className?: string;
+}
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// export default function Root(props) {
+//   return <section>{props.name} is mounted!</section>;
+// }
+
+// import "./styles/globals.css?modules=false";
+
+// You can also export React components from this file and import them into your microfrontends
+export { default as Button } from "./components/Button";
